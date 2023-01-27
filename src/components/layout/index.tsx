af317ab23cc94750c64671e6e1061b526bc66d3b
Flavio Lima <flaviogameover@gmail.com>
2023-01-27 14:05:33 -0300

    First build

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from '../navbar';
+
+type TLayout = {
+    children: React.ReactNode;
+}
+
+const index:React.FC<TLayout> = ({children}) => {
+    
+    return (
+        <>
+            <Navbar />
+            <main>{children}</main>
+        </>
+    )
+}
+export default index;
