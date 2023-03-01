9e8bbecca18713c49a18ee19df235b49aa9aa976
Flavio Lima <flaviogameover@gmail.com>
2023-03-01 14:20:12 -0300

    fixed path rightContent

--- src/components/Posts/Comments/CommentInput/index.tsx
+++ src/components/Posts/Comments/CommentInput/index.tsx
@@ -1,7 +1,7 @@
 import { Button, Flex, Text, Textarea } from '@chakra-ui/react';
 import { User } from 'firebase/auth';
 import React from 'react';
-import AuthButtons from '@rightContent/AuthButtons';
+import AuthButtons from '@/components/Navbar/RightContent/AuthButtons';
 
 type TCommentInput = {
 	commentText: string;
