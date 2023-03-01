1095ee6939099ffd8fc3d2c1dae52a069150c297
Flavio Lima <flaviogameover@gmail.com>
2023-03-01 14:57:24 -0300

    fixed chakra import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
-import ChakraComponent from './_chakraComponent';
+import ChakraComponent from './chakraComponent';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
